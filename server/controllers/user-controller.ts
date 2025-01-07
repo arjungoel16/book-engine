9fd37877a4ef24d8638425ce69720dcc968cd4da
arjungoel16 <arjun.goel6@gmail.com>
2025-01-07 18:38:38 -0500

    Create user-controller.ts

---
+++ server/controllers/user-controller.ts
@@ -0,0 +1,73 @@
+
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+module.exports = {
+  // get a single user by either their id or their username
+  async getSingleUser({ user = null, params }, res) {
+    const foundUser = await User.findOne({
+      $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+    });
+
+    if (!foundUser) {
+      return res.status(400).json({ message: 'Cannot find a user with this id!' });
+    }
+
+    res.json(foundUser);
+  },
+  async createUser({ body }, res) {
+    const user = await User.create(body);
+
+    if (!user) {
+      return res.status(400).json({ message: 'Something is wrong!' });
+    }
+    const token = signToken(user);
+    res.json({ token, user });
+  },
+
+  async login({ body }, res) {
+    // creating the user and waiting for user and password to be checked/created
+    const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+    if (!user) {
+        // if the user is not found, return an error
+      return res.status(400).json({ message: "Can't find this user" });
+    }
+
+    const correctPw = await user.isCorrectPassword(body.password);
+
+    if (!correctPw) {
+      return res.status(400).json({ message: 'Wrong password!' });
+    }
+    const token = signToken(user);
+    res.json({ token, user });
+  },
+
+  async saveBook({ user, body }, res) {
+    //function allows the user to save a book to their account
+    console.log(user);
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $addToSet: { savedBooks: body } },
+        { new: true, runValidators: true }
+      );
+      return res.json(updatedUser);
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json(err);
+    }
+  },
+  // remove a book from `savedBooks`
+  async deleteBook({ user, params }, res) {
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: user._id },
+      { $pull: { savedBooks: { bookId: params.bookId } } },
+      { new: true }
+    );
+    if (!updatedUser) {
+        // if the user is not found, return an error
+      return res.status(404).json({ message: "Couldn't find user with this id!" });
+    }
+    return res.json(updatedUser);
+  },
+};
