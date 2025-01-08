f6e77b128a6245843413b81306dfe2e30124f74f
arjungoel16 <arjun.goel6@gmail.com>
2025-01-07 19:22:21 -0500

    Add files via upload

---
+++ client/utils/mutations.js
@@ -0,0 +1,61 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+    $authors: [String]!
+    $title: String!
+    $bookId: ID!
+  ) {
+    saveBook(
+      authors: $authors
+      title: $title
+      bookId: $bookId
+    ) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        title
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        title
+      }
+    }
+  }
+`;
